922e954cbfbc804e64de68df253a205d5a33782a
Vid Kreca <vidkreca884@gmail.com>
2021-04-18 16:57:44 +0200

    Some additions in User class

--- CryptoSimulator/data/src/main/java/com/vidkreca/data/User.java
+++ CryptoSimulator/data/src/main/java/com/vidkreca/data/User.java
@@ -1,10 +1,17 @@
 package com.vidkreca.data;
 
 
+import java.util.ArrayList;
+
 public class User {
     private int id;
     private String username;
     private String email;
 
+    private double balanceFiat;
+    private ArrayList<Trade> trades;
 
+    public User(int id, String username, String email) {
+        // ...
+    }
 }
