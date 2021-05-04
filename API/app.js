6ac66143d24c9c4cb22e6552a7c07ab859677f3d
Vid Kreca <vidkreca884@gmail.com>
2021-05-04 17:27:21 +0200

    Revert "Added endpoints for fetching symbol prices and info"
    This reverts commit 10fbb2d555db815c47690fd8987918eb64b01908.

--- API/app.js
+++ API/app.js
@@ -4,17 +4,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var apiRouter = require('./routes/api');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', apiRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.send("error");
+  res.render('error');
 });
 
 module.exports = app;
